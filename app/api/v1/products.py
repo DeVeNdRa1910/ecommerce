5101535b5cb5d526c7a9f23a221c7ff8b9f22759
Devendra <devendra.k@webkorps.com>
2026-01-01 00:27:51 +0530

    Just changing the file structure

---
+++ app/api/v1/products.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, HTTPException, Query
+from service.products import get_all_products
+
+router = APIRouter()
+
+@router.get("/")
+def get_all_product():
+    return get_all_products()
+
+# /products?name="samsung"
+@router.get("/by-name")
+def get_product_by_name(
+    name: str = Query(
+        default=None, 
+        min_length=1, 
+        max_length=50, 
+        description="Search product by name (case insensitive)"
+    ),
+    sort_by_price: bool = Query(
+        default=False,
+        description="Sort products by price",
+    ),
+    order: str = Query(
+        default="asc",
+        description="Sort order when sort_by_price=true (asc, desc)"
+    ),
+    limit: int = Query(
+        default=10,
+        ge=1,
+        le=100,
+        description="Number of items"
+    ), 
+    offset: int = Query(
+        default=0,
+        ge=0, 
+        description="Pagination offset"  
+    )
+):
+    products = get_all_products()
+    
+    if name:
+        needle = name.strip().lower()
+        products = [p for p in products if needle in p.get("name", "").lower()]
+        
+        if not products:
+            raise HTTPException(status_code=404, detail=f"No product found matching name={name}")
+        total = len(products)
+        
+    if sort_by_price:
+        reverse = order == "desc"
+        products = sorted(products, key=lambda p: p.get("price", ""), reverse=reverse)
+        
+    products = products[offset: offset+limit]
+    
+    return {
+        "total": total,
+        "products": products
+    }
+    
