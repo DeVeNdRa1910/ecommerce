f1ea28c0941263bcce7685de1de3a6375d94cc3b
Devendra <devendra.k@webkorps.com>
2026-01-06 02:52:34 +0530

    Resolve some error

--- app/schemas.py
+++ app/schemas.py
@@ -16,7 +16,7 @@ from datetime import datetime
 
 # Create pydantic
 class Seller(BaseModel):
-    id: UUID
+    id: UUID = Field(alias="seller_id")
     name: str = Field(..., min_length=4)
     website: AnyUrl
     email: EmailStr  # Here we want to validate the email field only for that we can use field validator
@@ -33,8 +33,8 @@ class Seller(BaseModel):
     
 class Dimension(BaseModel):
     length: Annotated[float, Field(gt=0, strict=True, description="Length in cm")]
-    width: Annotated[float, Field(gt=0, strict=True, description="Length in cm")]
-    hieght: Annotated[float, Field(gt=0, strict=True, description="Length in cm")]
+    width: Annotated[float, Field(gt=0, strict=True, description="Width in cm")]
+    height: Annotated[float, Field(gt=0, strict=True, description="Height in cm")]
 
 class Product(BaseModel):
     id: UUID
@@ -59,7 +59,7 @@ class Product(BaseModel):
         Optional[List[str]],
         Field(default=None, max_length=10, description="Up to 10 tags ")
     ]
-    images: Annotated[
+    image_urls: Annotated[
         Optional[List[AnyUrl]],
         Field(min_length=1, description="Atleas 1 image url")
     ]
@@ -105,20 +105,27 @@ class Product(BaseModel):
     @property
     def calculate_volume(self) -> float:
         d = self.dimensions_cm
-        return round(d.hieght * d.length * d.width, 2)
+        return round(d.height * d.length * d.width, 2)
     
     
 # Update pydantic
 class SellerUpdate(BaseModel):
-    name: Optional[str] = Field(..., min_length=4)
-    website: Optional[AnyUrl]
-    email: Optional[EmailStr]  # Here we want to validate the email field only for that we can use field validator
+    name: Optional[str] = Field(None, min_length=4)
+    website: Optional[AnyUrl] = None
+    email: Optional[EmailStr] = None  # Here we want to validate the email field only for that we can use field validator
     
     
     @field_validator("email", mode="after")
     @classmethod
-    def validate_seller_email_domain(cls, value: EmailStr):
-        allwed_domain = ["mistore.in", "realmeofficial.in", "samsungindia.in", "lenovostore.in", "hpworld.in", "applestoreindia.in", "dellexclusive.in", "sonycenter.in", "oneplusstore.in", "asusexclusive.in", "gmail.com"]
+    def validate_seller_email_domain(cls, value: Optional[EmailStr]):
+        if value is None:
+            return value
+        allwed_domain = [
+            "mistore.in", "realmeofficial.in", "samsungindia.in",
+            "lenovostore.in", "hpworld.in", "applestoreindia.in",
+            "dellexclusive.in", "sonycenter.in", "oneplusstore.in",
+            "asusexclusive.in", "gmail.com"
+        ]
         domain = str(value).split("@")[-1].lower()
         if domain not in allwed_domain:
             raise ValueError(f"Seller email domain not allowed: {domain}") 
@@ -126,8 +133,8 @@ class SellerUpdate(BaseModel):
     
 class DimensionUpdate(BaseModel):
     length: Annotated[Optional[float], Field(gt=0, strict=True, description="Length in cm")] = None
-    width: Annotated[Optional[float], Field(gt=0, strict=True, description="Length in cm")] = None
-    hieght: Annotated[Optional[float], Field(gt=0, strict=True, description="Length in cm")] = None
+    width: Annotated[Optional[float], Field(gt=0, strict=True, description="Width in cm")] = None
+    height: Annotated[Optional[float], Field(gt=0, strict=True, description="Height in cm")] = None
 
 class ProductUpdate(BaseModel):
     name: Optional[str] = Field(None, min_length=4)
@@ -141,30 +148,17 @@ class ProductUpdate(BaseModel):
     is_active: Optional[bool] = None
     rating: Optional[float] = None
     tags: Optional[List[str]] = None
-    images: Optional[List[AnyUrl]] = None
+    image_urls: Optional[List[AnyUrl]] = None
     dimensions_cm: Optional[DimensionUpdate] = None
     seller: Optional[SellerUpdate] = None
 
     # model validator is working on the complete model
     @model_validator(mode="after")
     @classmethod
-    def validate_business_rules(cls, model: "Product"):
+    def validate_business_rules(cls, model: "ProductUpdate"):
         if model.stock == 0 and model.is_active is True:
             raise ValueError("If stock is zero then is_active must be false")
-        if model.discount_percent and model.discount_percent > 0: 
-            if model.discount_percent > 0 and model.rating == 0:
+        if model.discount_percent and model.discount_percent > 0:
+            if model.rating is not None and model.rating == 0:
                 raise ValueError("Discounted product must have rating")
-        
         return model
-    
-    # For final price we have to substract the discount from the price. For that kind of work we have compute_field
-    @computed_field
-    @property
-    def calculate_price_after_discount(self) -> float:
-        return round(self.price * (1 - self.discount_percent / 100), 2)
-    
-    @computed_field
-    @property
-    def calculate_volume(self) -> float:
-        d = self.dimensions_cm
-        return round(d.hieght * d.length * d.width, 2)
